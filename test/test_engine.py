e3d498d7d610d37f7211491632c99f29c7b5f6b7
sharan parikh <parikh.shar@northeastern.edu>
2023-10-31 17:45:38 -0400

    Added unit test cases for all engines and batteries

---
+++ test/test_engine.py
@@ -0,0 +1,43 @@
+import unittest
+
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 30002
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 29999
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+class TestSternmanEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 60001
+        last_service_mileage = 0
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 59999
+        last_service_mileage = 0
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
