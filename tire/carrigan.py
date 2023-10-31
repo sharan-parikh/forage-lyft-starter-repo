0034994933dcdeaf9a02683ee451476a71c4b1de
sharan parikh <parikh.shar@northeastern.edu>
2023-10-31 19:58:11 -0400

    Tire implementations and spindler service logic update

---
+++ tire/carrigan.py
@@ -0,0 +1,9 @@
+from tire.model.tire import Tires
+
+class CarriganTires(Tires):
+    def __init__(self, tire_wear_readings):
+        super().__init__()
+        self.tire_wear_readings = tire_wear_readings
+
+    def needs_service(self):
+        return any(x for x in self.tire_wear_readings if x >= 0.9)
---
+++ tire/octoprime.py
@@ -0,0 +1,10 @@
+from tire.model.tires import Tires
+
+
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear_readings):
+        super().__init__()
+        self.tire_wear_readings = tire_wear_readings
+
+    def needs_service(self):
+        return sum(self.tire_wear_readings) >= 3
